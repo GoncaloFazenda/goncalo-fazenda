cef1d9363bc1739f5138bda1e34c17339884e1fb
Gonçalo Fazenda <goncalo.fazenda@logirix.com>
2023-12-10 12:54:39 +0000

    env prod testing

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -2,8 +2,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
 import React, { useState } from 'react';
-import dotenv from 'dotenv';
-dotenv.config();
 
 import { trpc } from './client';
 
@@ -13,7 +11,7 @@ export default function Provider({ children }: { children: React.ReactNode }) {
         trpc.createClient({
             links: [
                 httpBatchLink({
-                    url: `${process.env.DOMAIN}/api/trpc`,
+                    url: process.env.NEXT_PUBLIC_DOMAIN + '/api/trpc',
                 }),
             ],
         })
