93836813a13d2f3ce42170b0e39319e788b4b368
Gonçalo Fazenda <goncalo.fazenda@logirix.com>
2023-12-13 11:42:50 +0000

    TRPC fail in deploy test

--- src/app/_components/fetchComponent.tsx
+++ src/app/_components/fetchComponent.tsx
@@ -1,18 +1,36 @@
 'use client';
+import { useState } from 'react';
 import { trpc } from '../_trpc/client';
 
 export default function FetchComponent() {
-    const getMessage = trpc.getUsers.useQuery();
+    const [name, setName] = useState<string>('');
+    const getList = trpc.getUsers.useQuery();
     const addUser = trpc.addUser.useMutation();
-    getMessage.isError && console.log(getMessage.error);
+    const deleteAllUsers = trpc.deleteAllUsers.useMutation();
 
     return (
         <div>
-            <h1>Fetch Component</h1>
-            <p>{JSON.stringify(getMessage.data)}</p>
-            {getMessage.isError && <h1 className=" text-3xl text-red-500">{JSON.stringify(getMessage.error)}</h1>}
-            <button onClick={() => addUser.mutate('fazenda')}>Add user</button>
-            <p>{JSON.stringify(addUser.data)}</p>
+            <h1>getList Error goes under this</h1>
+            {getList.isError && <h1 className=" text-3xl text-red-500">{JSON.stringify(getList.error)}</h1>}
+            <h1>& above this</h1>
+            <br />
+            <div className="flex flex-col space-y-8 ">
+                <input type="text" onChange={(e) => setName(e.target.value)} />
+                <button
+                    className=" p-4 bg-red-400 rounded-2xl border-2 border-white text-lg"
+                    onClick={() => addUser.mutate(name)}
+                >
+                    Add user
+                </button>
+            </div>
+            <br />
+            <p className="text-blue">list: {JSON.stringify(getList.data)}</p>
+            <br />
+            <p className="text-white">last name submited: {addUser.data && JSON.stringify(addUser.data)}</p>
+            <br />
+            <button className=" p-4 bg-red-400 rounded-2xl border-2 border-white text-lg" onClick={() => deleteAllUsers.mutate()}>
+                Delete all
+            </button>
         </div>
     );
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,31 +1,37 @@
 import { publicProcedure, router } from './trpc';
-import { drizzle } from 'drizzle-orm/postgres-js';
-import { migrate } from 'drizzle-orm/postgres-js/migrator';
+// import { drizzle } from 'drizzle-orm/postgres-js';
+// import { migrate } from 'drizzle-orm/postgres-js/migrator';
 // import * as schema from '@/db/schema';
-import postgres from 'postgres';
+// import postgres from 'postgres';
 import { z } from 'zod';
-// Schemas in use
 // const { users } = schema;
 
 if (!process.env.DATABASE_URL) {
     console.error('DATABASE_URL env var is not set');
 }
 
-const queryClient = postgres(process.env.DATABASE_URL!, { max: 1 });
-const db = drizzle(queryClient);
+// const queryClient = postgres(process.env.DATABASE_URL!, { max: 1 });
+// const db = drizzle(queryClient);
 
 // Drizzle pushes the already generated migrations to the database only if needed. Note:(this will throw an error if the ./drizzle/meta/_journal.json file does not exist)
-migrate(db, { migrationsFolder: 'drizzle' });
+// if (process.env.NODE_ENV === 'production') migrate(db, { migrationsFolder: 'drizzle' });
+
+let storedUsers: any = [];
 
 export const appRouter = router({
     getUsers: publicProcedure.query(async () => {
         // return await db.select().from(users);
-        return { message: 'Hello World' };
+        return storedUsers;
     }),
     addUser: publicProcedure.input(z.string()).mutation(async (params) => {
         const { input } = params;
         // await db.insert(users).values({ name: input });
+        storedUsers.push(input);
         return input;
+    }),
+    deleteAllUsers: publicProcedure.mutation(async () => {
+        // await db.delete(users);
+        storedUsers = [];
     }),
 });
 
